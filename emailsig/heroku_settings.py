480b4b975eec85da8c3be7f8661b30cc052019ed
charliequinn <charlesquinn1984@gmail.com>
2014-10-16 11:43:02 +0100

    update db settings

--- emailsig/heroku_settings.py
+++ emailsig/heroku_settings.py
@@ -2,4 +2,4 @@ from .settings import *
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
