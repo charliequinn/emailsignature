9c6c1491def76759b05d8820b8f89ff29e712000
Ioannis Cleary <ioanniscleary@gmail.com>
2018-02-19 11:21:13 +0000

    UP-671 UP-672 template adjusts to location and lack of info

--- emailsig/forms.py
+++ emailsig/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class EmailSignatureForm(forms.Form):
+    LOCATION_CHOICES = (
+        ('', '--------'),
+        ('usa', 'USA'),
+        ('other', 'Rest of World'),
+    )
+
     full_name = forms.CharField(label='Full name *', max_length=40, required=True)
     job_title = forms.CharField(label='Job title *', max_length=40, required=True)
     main_contact_number = forms.CharField(label='Office number *', max_length=15, required=True)
@@ -9,5 +15,4 @@ class EmailSignatureForm(forms.Form):
     address_line_2 = forms.CharField(label='Address line 2 (City)', max_length=30, required=False)
     address_line_3 = forms.CharField(label='Address line 3 (Postcode)', max_length=15, required=False)
     website = forms.CharField(label='Website', max_length=70, required=False, initial="www.twigeducation.com")
-
-
+    location = forms.ChoiceField(choices=LOCATION_CHOICES, label='Location *')
